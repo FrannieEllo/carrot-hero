617e077cb1012dc99b463784624a636780138ac1
FrannieEllo <fello@uw.edu>
2023-03-08 20:47:56 -0800

    help, now  my local server is not running after adding to my package.json O_O

--- vite.js
+++ vite.js
@@ -2,16 +2,11 @@
 
 import { defineConfig } from 'vite'
 export default defineConfig({
-    module.exports = {
-    input: "index.js",
-    output: {
-        dir: "dist",
-    },
-    plugins: [
-        copy({
-        targets: [{ src: ["index.html", "assets"], dest: "dist" }],
-        }),
-        nodeResolve(),
-    ],
-    };
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html'
+            }
+        }
+    }
 })
